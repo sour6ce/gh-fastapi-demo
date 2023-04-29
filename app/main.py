1d0696f9f33214cc5451c39011ab1faf803c387d
Gabriel Herrod <gabrielhdezrodriguez@gmail.com>
2023-04-29 15:30:58 -0400

    feat: Models definition

--- app/main.py
+++ app/main.py
@@ -1,4 +1,36 @@
+from enum import StrEnum
+from typing import List
+
 from fastapi import FastAPI
+from pydantic import BaseModel, validator
+
+# region Models
+
+Status = StrEnum('Status', 'completed pending canceled')
+
+Criterion = StrEnum('Criterion', 'completed pending canceled all')
+
+
+class Order(BaseModel):
+    id: int
+    item: str
+    quantity: int
+    price: float
+    status: Status
+
+    @validator('price')
+    def check_price_not_negative(cls, price: float) -> float:
+        if price < 0:
+            raise ValueError('Price value mut be greater than or equal to 0')
+        return price
+
+
+class ProcessModel(BaseModel):
+    orders: List[Order]
+    criterion: Criterion
+
+# endregion
+
 
 app = FastAPI()
 
